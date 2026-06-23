9f0927077ee06e4228e0045ee250a7484a4341a7
GHC199610 <huancong.g@gmail.com>
2026-06-23 12:53:17 +0800

    Add installable offline PWA support

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'toilet-partition-v2pro-20260623-1';
+const APP_ASSETS = [
+  './',
+  './toilet_partition_auto_generator_V2Pro.html',
+  './manifest.webmanifest',
+  './pwa-icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => key === CACHE_NAME ? null : caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match('./toilet_partition_auto_generator_V2Pro.html')))
+  );
+});
